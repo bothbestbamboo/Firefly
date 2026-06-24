89516085d7dcc765c48a3c725d9041a8ac8d9169
bothbestbamboo <93992333@qq.com>
2026-06-25 07:23:25 +0800

    update footer

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -23,29 +23,29 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
-		title: "夏夜流萤",
+		title: "bamboo table top",
 		imgurl:
-			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-		siteurl: "https://blog.cuteleaf.cn",
+			"https://www.bambooindustry.com/images/flooring-banner.jpg",
+		desc: "Bamboo flooring has a very wide varieties, including solid, engineered, strand woven, click lock, click strand, heating, hand scraped, brushed, distressed, industrial parquet and flooring accessories.",
+		siteurl: "https://www.bambooindustry.com/blog/bamboo-standing-tabletop-desktops.html",
 		tags: ["Blog"],
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
 	{
-		title: "Firefly Docs",
-		imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-		desc: "Firefly主题模板文档",
-		siteurl: "https://docs-firefly.cuteleaf.cn",
+		title: "bamboo plywood",
+		imgurl: "https://www.bambooindustry.com/images/plywood-banner.jpg",
+		desc: "Bamboo means natural. Each plywood then can slightly differ both in its structure and colour. It consists of numerous slats compressed together - each of the slats of thousands of strands. That's why it has such a non-uniform, organic structure - and this is what makes the material exceptional.",
+		siteurl: "https://www.bambooindustry.com/products/bamboo-panel/bamboo-plywood.html",
 		tags: ["Docs"],
 		weight: 9,
 		enabled: true,
 	},
 	{
-		title: "Astro",
-		imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-		desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-		siteurl: "https://github.com/withastro/astro",
+		title: "bamboo decking",
+		imgurl: "https://www.bambooindustry.com/images/decking-banner.jpg",
+		desc: "Bamboo decking is an eco-friendly and environmental deck. It is an ideal alternative for traditional hardwood decking and WPC decking. Bamboo decking brings you a new idea to make real green choice, merging functionality with aesthetics for a spectacular and durable outdoor space.",
+		siteurl: "https://www.bambooindustry.com/products/bamboo-flooring/bamboo-decking.html",
 		tags: ["Framework"],
 		weight: 8,
 		enabled: true,
