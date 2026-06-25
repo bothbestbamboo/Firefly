5e4ac4d14b378a5da1b4d6084501efcdb161600b
bothbestbamboo <93992333@qq.com>
2026-06-25 11:40:37 +0800

    creat new post

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/announcementConfig";
 
 export const announcementConfig: AnnouncementConfig = {
 	// 公告标题
-	title: "公告",
+	title: "Profile",
 
 	// 公告内容
 	content: "Bothbest is a professional factory of bamboo flooring in China, dedicated to manufacturing and exporting premium solid and engineered bamboo products worldwide. ",
@@ -14,7 +14,7 @@ export const announcementConfig: AnnouncementConfig = {
 		// 启用链接
 		enable: true,
 		// 链接文本
-		text: "了解更多",
+		text: "More",
 		// 链接 URL
 		url: "/about/",
 		// 内部链接
